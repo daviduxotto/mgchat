b4b566c434cb645f525ffc2cf9d6d1be9313dc2f
Daviduxotto <daviduxotto@gmail.com>
2016-04-12 16:48:37 -0600

    Iniciacion
    creacion del proyecto inicial

---
+++ app/src/main/java/com/example/davidvelasquez/mgchat/Principal.java
@@ -0,0 +1,16 @@
+package com.example.davidvelasquez.mgchat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class Principal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+
+
+    }
+}
