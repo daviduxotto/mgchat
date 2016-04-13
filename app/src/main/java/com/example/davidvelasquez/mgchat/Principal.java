0b40406ed2680e361d0b166f50cdf275ee3148f4
Daviduxotto <daviduxotto@gmail.com>
2016-04-12 18:01:25 -0600

    pre-inicio -session
    ya esta la classe que llama para verificar si incia sesion listo para
    llamar la ventana de inicio o registro

--- app/src/main/java/com/example/davidvelasquez/mgchat/Principal.java
+++ app/src/main/java/com/example/davidvelasquez/mgchat/Principal.java
@@ -5,12 +5,38 @@ import android.os.Bundle;
 import android.widget.Toast;
 
 public class Principal extends AppCompatActivity {
+    String username;
+    String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+        if(iniciar_session()==true)
+        {
+            Toast.makeText(this, "Iniciado Session", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Toast.makeText(this,"No inicio session", Toast.LENGTH_LONG).show();
+        }
 
+
+    }
+
+    public boolean iniciar_session() // iniciar sesion-------------------
+    {
+        Datos datos = new Datos();
+        String [] sdatos = datos.leer(this);
+       if(sdatos[0]!="" && sdatos[1]!="") // si no esta vacio
+        {
+            username= sdatos[0];
+            password= sdatos[1];
+            return true;
+        }
+        else {
+           return false;
+       }
 
     }
 }
