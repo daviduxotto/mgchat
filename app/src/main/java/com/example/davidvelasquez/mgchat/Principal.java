1d781966ddf54e2883d425986aee5a1797c1779c
Daviduxotto <daviduxotto@gmail.com>
2016-04-18 18:51:37 -0600

    Revert "pre-inicio -session"
    This reverts commit 0b40406ed2680e361d0b166f50cdf275ee3148f4.
    
    # Conflicts:
    #	.idea/vcs.xml
    #	.idea/workspace.xml
    #	app/src/main/java/com/example/davidvelasquez/mgchat/Principal.java
    
    intento de material tabsa

--- app/src/main/java/com/example/davidvelasquez/mgchat/Principal.java
+++ app/src/main/java/com/example/davidvelasquez/mgchat/Principal.java
@@ -1,17 +1,45 @@
 package com.example.davidvelasquez.mgchat;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Principal extends AppCompatActivity {
+<<<<<<< HEAD
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
     String username;
     String password;
+=======
+>>>>>>> parent of 0b40406... pre-inicio -session
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+<<<<<<< HEAD
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
         if(iniciar_session()==true)
         {
             Toast.makeText(this, "Iniciado Session", Toast.LENGTH_SHORT).show();
@@ -20,9 +48,14 @@ public class Principal extends AppCompatActivity {
         {
             Toast.makeText(this,"No inicio session", Toast.LENGTH_LONG).show();
         }
+=======
+>>>>>>> parent of 0b40406... pre-inicio -session
+
+
 
 
     }
+<<<<<<< HEAD
 
     public boolean iniciar_session() // iniciar sesion-------------------
     {
@@ -39,4 +72,46 @@ public class Principal extends AppCompatActivity {
        }
 
     }
+
+
+
+    // funciones para los tab material
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new inicio(), "ONE");
+
+        viewPager.setAdapter(adapter);
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
 }
+=======
+}
+>>>>>>> parent of 0b40406... pre-inicio -session
---
+++ app/src/main/java/com/example/davidvelasquez/mgchat/inicio.java
@@ -0,0 +1,29 @@
+package com.example.davidvelasquez.mgchat;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * Created by David Velasquez on 13/04/2016.
+ */
+public class inicio extends  Fragment {
+    public  inicio() { // constructor
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.inicio, container, false);
+    }
+
+}
